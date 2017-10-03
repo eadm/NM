ec8a6e70f0f54679109d75ce654adba561cf003e
Артём Павленко <lytr777@MacBook-Pro-Artem.local>
2017-10-04 01:40:22 +0300

    ref

--- NM_HW_ODE/runge_kutta_method.py
+++ NM_HW_ODE/runge_kutta_method.py
@@ -5,60 +5,10 @@ def solve(system, conditions):
     xi, yi, zi = conditions
     xs, ys, zs, ts = [xi], [yi], [zi], [t]
 
-    k0 = {
-        "x": lambda x, y, z: system["x"](x, y, z),
-        "y": lambda x, y, z: system["y"](x, y, z),
-        "z": lambda x, y, z: system["z"](x, y, z)
-    }
-    k1 = {
-        "x": lambda x, y, z: system["x"](x + (dt * k0["x"](x, y, z) / 2),
-                                         y + (dt * k0["y"](x, y, z) / 2),
-                                         z + (dt * k0["z"](x, y, z) / 2)
-                                         ),
-        "y": lambda x, y, z: system["y"](x + (dt * k0["x"](x, y, z) / 2),
-                                         y + (dt * k0["y"](x, y, z) / 2),
-                                         z + (dt * k0["z"](x, y, z) / 2)
-                                         ),
-        "z": lambda x, y, z: system["z"](x + (dt * k0["x"](x, y, z) / 2),
-                                         y + (dt * k0["y"](x, y, z) / 2),
-                                         z + (dt * k0["z"](x, y, z) / 2)
-                                         )
-    }
-    k2 = {
-        "x": lambda x, y, z: system["x"](x + (dt * k1["x"](x, y, z) / 2),
-                                         y + (dt * k1["y"](x, y, z) / 2),
-                                         z + (dt * k1["z"](x, y, z) / 2)
-                                         ),
-        "y": lambda x, y, z: system["y"](x + (dt * k1["x"](x, y, z) / 2),
-                                         y + (dt * k1["y"](x, y, z) / 2),
-                                         z + (dt * k1["z"](x, y, z) / 2)
-                                         ),
-        "z": lambda x, y, z: system["z"](x + (dt * k1["x"](x, y, z) / 2),
-                                         y + (dt * k1["y"](x, y, z) / 2),
-                                         z + (dt * k1["z"](x, y, z) / 2)
-                                         )
-    }
-    k3 = {
-        "x": lambda x, y, z: system["x"](x + dt * k1["x"](x, y, z),
-                                         y + dt * k1["y"](x, y, z),
-                                         z + dt * k1["z"](x, y, z)
-                                         ),
-        "y": lambda x, y, z: system["y"](x + dt * k1["x"](x, y, z),
-                                         y + dt * k1["y"](x, y, z),
-                                         z + dt * k1["z"](x, y, z)
-                                         ),
-        "z": lambda x, y, z: system["z"](x + dt * k1["x"](x, y, z),
-                                         y + dt * k1["y"](x, y, z),
-                                         z + dt * k1["z"](x, y, z)
-                                         )
-    }
-
-    k = lambda v, x, y, z: (k0[v](x, y, z) + 2 * k1[v](x, y, z) + 2 * k2[v](xi, yi, zi) + k3[v](x, y, z)) / 6
-
     for _ in range(500):
-        xi1 = xi + k("x", xi, yi, zi) * dt
-        yi1 = yi + k("y", xi, yi, zi) * dt
-        zi1 = zi + k("z", xi, yi, zi) * dt
+        xi1 = xi + k(system, "x", xi, yi, zi, dt) * dt
+        yi1 = yi + k(system, "y", xi, yi, zi, dt) * dt
+        zi1 = zi + k(system, "z", xi, yi, zi, dt) * dt
 
         xi, yi, zi, t = xi1, yi1, zi1, t + dt
 
@@ -68,3 +18,30 @@ def solve(system, conditions):
         ts.append(t)
 
     return xs, ys, zs, ts
+
+
+def k(system, v, x, y, z, dt):
+    return (k0(system, v, x, y, z, dt) + 2 * k1(system, v, x, y, z, dt) +
+            2 * k2(system, v, x, y, z, dt) + k3(system, v, x, y, z, dt)) / 6
+
+
+def k0(system, v, x, y, z, dt):
+    return system[v](x, y, z)
+
+
+def k1(system, v, x, y, z, dt):
+    return system[v](x + (dt * k0(system, "x", x, y, z, dt) / 2),
+                     y + (dt * k0(system, "y", x, y, z, dt) / 2),
+                     z + (dt * k0(system, "z", x, y, z, dt) / 2))
+
+
+def k2(system, v, x, y, z, dt):
+    return system[v](x + (dt * k1(system, "x", x, y, z, dt) / 2),
+                     y + (dt * k1(system, "y", x, y, z, dt) / 2),
+                     z + (dt * k1(system, "z", x, y, z, dt) / 2))
+
+
+def k3(system, v, x, y, z, dt):
+    return system[v](x + dt * k2(system, "x", x, y, z, dt),
+                     y + dt * k2(system, "y", x, y, z, dt),
+                     z + dt * k2(system, "z", x, y, z, dt))

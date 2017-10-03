207c92d8190f2a96d974827abe882204346afc64
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-10-04 01:51:38 +0300

    clean up

--- NM_HW_ODE/runge_kutta_method.py
+++ NM_HW_ODE/runge_kutta_method.py
@@ -6,9 +6,9 @@ def solve(system, conditions):
     xs, ys, zs, ts = [xi], [yi], [zi], [t]
 
     for _ in range(500):
-        xi1 = xi + k(system, "x", xi, yi, zi, dt) * dt
-        yi1 = yi + k(system, "y", xi, yi, zi, dt) * dt
-        zi1 = zi + k(system, "z", xi, yi, zi, dt) * dt
+        xi1 = xi + __k(system, "x", xi, yi, zi, dt) * dt
+        yi1 = yi + __k(system, "y", xi, yi, zi, dt) * dt
+        zi1 = zi + __k(system, "z", xi, yi, zi, dt) * dt
 
         xi, yi, zi, t = xi1, yi1, zi1, t + dt
 
@@ -20,28 +20,28 @@ def solve(system, conditions):
     return xs, ys, zs, ts
 
 
-def k(system, v, x, y, z, dt):
-    return (k0(system, v, x, y, z, dt) + 2 * k1(system, v, x, y, z, dt) +
-            2 * k2(system, v, x, y, z, dt) + k3(system, v, x, y, z, dt)) / 6
+def __k(system, v, x, y, z, dt):
+    return (__k0(system, v, x, y, z, dt) + 2 * __k1(system, v, x, y, z, dt) +
+            2 * __k2(system, v, x, y, z, dt) + __k3(system, v, x, y, z, dt)) / 6
 
 
-def k0(system, v, x, y, z, dt):
+def __k0(system, v, x, y, z, dt):
     return system[v](x, y, z)
 
 
-def k1(system, v, x, y, z, dt):
-    return system[v](x + (dt * k0(system, "x", x, y, z, dt) / 2),
-                     y + (dt * k0(system, "y", x, y, z, dt) / 2),
-                     z + (dt * k0(system, "z", x, y, z, dt) / 2))
+def __k1(system, v, x, y, z, dt):
+    return system[v](x + (dt * __k0(system, "x", x, y, z, dt) / 2),
+                     y + (dt * __k0(system, "y", x, y, z, dt) / 2),
+                     z + (dt * __k0(system, "z", x, y, z, dt) / 2))
 
 
-def k2(system, v, x, y, z, dt):
-    return system[v](x + (dt * k1(system, "x", x, y, z, dt) / 2),
-                     y + (dt * k1(system, "y", x, y, z, dt) / 2),
-                     z + (dt * k1(system, "z", x, y, z, dt) / 2))
+def __k2(system, v, x, y, z, dt):
+    return system[v](x + (dt * __k1(system, "x", x, y, z, dt) / 2),
+                     y + (dt * __k1(system, "y", x, y, z, dt) / 2),
+                     z + (dt * __k1(system, "z", x, y, z, dt) / 2))
 
 
-def k3(system, v, x, y, z, dt):
-    return system[v](x + dt * k2(system, "x", x, y, z, dt),
-                     y + dt * k2(system, "y", x, y, z, dt),
-                     z + dt * k2(system, "z", x, y, z, dt))
+def __k3(system, v, x, y, z, dt):
+    return system[v](x + dt * __k2(system, "x", x, y, z, dt),
+                     y + dt * __k2(system, "y", x, y, z, dt),
+                     z + dt * __k2(system, "z", x, y, z, dt))

c6e2ed46d207d06089f0083873cc84e79a4c184f
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-12-20 01:17:25 +0300

    plots

--- NM_HW_TEMP/main.py
+++ NM_HW_TEMP/main.py
@@ -1,2 +1,25 @@
 import numpy as np
+import explicit_flow_method
+import pylab as pl
+import explicit_counter_flow_method
 
+cs = explicit_flow_method.solve(
+    lambda t, x, T: 1.3,
+    lambda t, x, T: 0.,
+    {
+        "t_min": 0.,
+        "t_max": 20.,
+        "dt": 0.2,
+        "dx": 1.,
+        "T": lambda x: x ** 2,
+        "x_min": 0.,
+        "x_max": 20.
+    }
+)
+
+m_x, m_y = np.mgrid[slice(0., 20., 1.), slice(0., 20., 0.2)]
+
+
+pl.pcolormesh(m_x, m_y, cs)
+pl.show()
+print cs
